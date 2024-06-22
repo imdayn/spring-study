894bb4b6d8257023db586a237bfcace4203dcaac
onsundayn <onsundayn@gmail.com>
2024-06-23 01:11:04 +0900

    set

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+
+    private static final Map<Long, Member> store = new HashMap<>();
+    // 실무에서는 동시성 이슈로 Concurrent 해시맵을 사용함
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
